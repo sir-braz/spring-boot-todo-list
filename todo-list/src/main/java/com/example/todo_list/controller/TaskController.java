57c4e87ee137f8b9e199a6e3126bc41c3603f80d
sir-braz <mrbrazdev@gmail.com>
2024-08-10 15:43:28 -0500

    feat: add endpoint to retrieve task by ID

--- todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
+++ todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.example.todo_list.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class TaskController {
 
     @GetMapping("/tasks")
     public ResponseEntity<List<Task>> listAllTasks(){
+        return null;
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> listTaskById(@PathVariable Long id){
         return null;
     }
 
