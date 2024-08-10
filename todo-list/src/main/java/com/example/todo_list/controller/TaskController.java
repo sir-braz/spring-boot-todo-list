5b200a84044e9913fdd92bbba4d9247f23c26ddb
sir-braz <mrbrazdev@gmail.com>
2024-08-10 15:39:24 -0500

    feat: add basic REST controller for tasks

--- todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
+++ todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
@@ -1,4 +1,16 @@
 package com.example.todo_list.controller;
 
+import com.example.todo_list.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+
 }
