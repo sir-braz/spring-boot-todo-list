e144f32d274efd7e4755dac3dcfa91d5eef48e86
sir-braz <mrbrazdev@gmail.com>
2024-08-10 15:45:13 -0500

    feat: add endpoint to save a task

--- todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
+++ todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
@@ -4,10 +4,7 @@ import com.example.todo_list.entity.Task;
 import com.example.todo_list.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,11 @@ public class TaskController {
 
     @GetMapping("/tasks/{id}")
     public ResponseEntity<Task> listTaskById(@PathVariable Long id){
+        return null;
+    }
+
+    @PostMapping("/tasks")
+    public ResponseEntity<Task> saveTask(@RequestBody Task task){
         return null;
     }
 
