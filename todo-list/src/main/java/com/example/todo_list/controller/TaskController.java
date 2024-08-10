0acc301e3c44533997f908b81a98acc0d4c4d0b9
sir-braz <mrbrazdev@gmail.com>
2024-08-10 15:41:04 -0500

    feat: add endpoint to list all tasks

--- todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
+++ todo-list/src/main/java/com/example/todo_list/controller/TaskController.java
@@ -1,9 +1,14 @@
 package com.example.todo_list.controller;
 
+import com.example.todo_list.entity.Task;
 import com.example.todo_list.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +16,11 @@ public class TaskController {
 
     @Autowired
     private TaskService taskService;
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> listAllTasks(){
+        return null;
+    }
 
 
 }
